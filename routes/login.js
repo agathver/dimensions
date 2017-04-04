667287edd73161aaa53c85a7f739faf42e5d77cc
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-04-04 19:05:37 +0530

    Add user model

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const userlib = require('../lib/user');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.render('login.html.twig');
 });
 
-router.post('/', function (req, res) {
+router.post('/', (req, res) => {
   let username = req.body['username'];
   let password = req.body['username'];
 
-  res.render('login.html.twig', {
-    error: 'Username or password incorrect'
+  userlib.login(username, password).then((user) => {
+    // login successful
+    res.redirect('/dashboard');
+  }).catch((err) => {
+    res.render('login.html.twig', {
+      error: 'Username or password incorrect'
+    });
   });
 });
 
