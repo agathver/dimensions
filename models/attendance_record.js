45844c420f9cb33e3dfc9da28d87dd8aaa20df9b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-04-09 17:17:31 +0530

    Add lint task

--- models/attendance_record.js
+++ models/attendance_record.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    userId: String,
-    password: String
+  name: String,
+  userId: String,
+  password: String
 });
 
 module.exports = mongoose.model('User', userSchema);
--- models/event.js
+++ models/event.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    place: String,
-    date: Date,
-    image: String,
-    link: String
+  name: String,
+  place: String,
+  date: Date,
+  image: String,
+  link: String
 });
 
 module.exports = mongoose.model('Event', userSchema);
