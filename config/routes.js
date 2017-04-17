b6965ef37b2abee4276d4ac729e13b66d33e0b37
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-04-17 13:34:40 +0530

    Fix logout and sidebar links

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,12 @@ const index = require('../routes/index');
 const admin = require('../routes/admin');
 const dashboard = require('../routes/dashboard');
 const login = require('../routes/login');
+const logout = require('../routes/logout');
 
 module.exports = function (app) {
   app.use('/', index);
   app.use('/login', login);
+  app.use('/logout', logout);
   app.use('/dashboard', dashboard);
   app.use('/admin', admin);
 };
